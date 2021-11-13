ad001ff124401970d8da6671c9d00290cac88779
ETweb51 <87807976+ETweb51@users.noreply.github.com>
2021-11-13 18:55:27 +0100

    4.4

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class NumberOfEvents extends Component {
+  
+  state = {
+    numberOfEvents: 32
+  }
+  
+  handleInputChanged = (event) => {
+    const number = event.target.value;
+    if (number <= 0 || number > 32) {
+      return window.alert('Choose number between 1-32');
+    } else {
+      this.setState({ 
+        numberOfEvents: number,
+        });
+        this.props.updateNumberOfEvents(event.target.value);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Change number of shown events between 1-32</p>
+        
+        <input type="number" id="numberInput" value={this.state.numberOfEvents} className="numberInput" onChange={() => this.handleInputChanged(e)} />
+      </div>
+    );
+  }
+}
+
+export default NumberOfEvents;
