b3f9e89be175e61b090cca50174320c69d7215bc
ETweb51 <87807976+ETweb51@users.noreply.github.com>
2021-11-21 16:21:04 +0100

    4.6.1

--- src/api.js
+++ src/api.js
@@ -19,6 +19,35 @@ const checkToken = async (accessToken) => {
   return result;
 };
 
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
+
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    'https://5lvsn6dgmj.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+  )
+  .then((res) => {
+    return res.json();
+  })
+  .catch((error) => error);
+  
+  access_token && localStorage.setItem("access_token", access_token);
+  
+  return access_token;
+};
+
 export const getEvents = async () => {
     NProgress.start();
   
@@ -63,33 +92,4 @@ export const getAccessToken = async () => {
       return code && getToken(code);
     }
   return accessToken;
-}
-
-const removeQuery = () => {
-  if (window.history.pushState && window.location.pathname) {
-    var newurl =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname;
-    window.history.pushState("", "", newurl);
-  } else {
-    newurl = window.location.protocol + "//" + window.location.host;
-    window.history.pushState("", "", newurl);
-  }
-};
-
-const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const { access_token } = await fetch(
-    'https://5lvsn6dgmj.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
-  )
-  .then((res) => {
-    return res.json();
-  })
-  .catch((error) => error);
-  
-  access_token && localStorage.setItem("access_token", access_token);
-  
-  return access_token;
 };
