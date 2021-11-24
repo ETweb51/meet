38516aa7d45d60dc85cb1af668bd9d156e8b169e
ETweb51 <87807976+ETweb51@users.noreply.github.com>
2021-11-24 17:10:36 +0100

    4.7.8

--- src/api.js
+++ src/api.js
@@ -22,7 +22,7 @@ const removeQuery = () => {
   }
 };
 
-const getToken = async (code) => {
+export const getToken = async (code) => {
   const encodeCode = encodeURIComponent(code);
   const { access_token } = await fetch(
     'https://5lvsn6dgmj.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
@@ -59,7 +59,7 @@ export const getAccessToken = async () => {
 }
 
 // Access token was found in local storage
-const checkToken = async (accessToken) => {
+export const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   )
